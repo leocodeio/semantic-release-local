5ccbf3cea3543a912e8fee6d6881cadce29a82d4
leocodeio <basicleo787@gmail.com>
2025-02-18 22:40:08 +0530

    feat(TWEAK the config for commit cz-commitlint): tweak

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,33 @@
 export default {
   extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat", // New feature
+        "fix", // Bug fix
+        "docs", // Documentation
+        "style", // Formatting, missing semicolons, etc
+        "refactor", // Code change that neither fixes a bug nor adds a feature
+        "test", // Adding tests
+        "chore", // Updating build tasks, package manager configs, etc
+        "perf", // Performance improvements
+        "revert", // Revert to a commit
+        "wip", // Work in progress
+      ],
+    ],
+    "type-case": [2, "always", "lower-case"],
+    "subject-case": [
+      2,
+      "never",
+      ["sentence-case", "start-case", "pascal-case", "upper-case"],
+    ],
+    "subject-empty": [2, "never"],
+    "subject-full-stop": [2, "never", "."],
+    "subject-min-length": [2, "always", 3],
+    "subject-max-length": [2, "always", 100],
+    "body-max-line-length": [2, "always", 100],
+    "footer-max-line-length": [2, "always", 100],
+  },
 };
