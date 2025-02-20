a676cba516e19e1d316f26f98c28d56b59275915
pre-commit <pre-commit@example.com>
2025-02-20 10:06:47 +0530

    feat(tes): test

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,33 +1,35 @@
 export default {
   extends: ["@commitlint/config-conventional"],
   rules: {
+    "references-empty": [2, "never"],
+    "subject-case": [0],
     "type-enum": [
       2,
       "always",
       [
-        "feat", // New feature
-        "fix", // Bug fix
-        "docs", // Documentation
-        "style", // Formatting, missing semicolons, etc
-        "refactor", // Code change that neither fixes a bug nor adds a feature
-        "test", // Adding tests
-        "chore", // Updating build tasks, package manager configs, etc
-        "perf", // Performance improvements
-        "revert", // Revert to a commit
-        "wip", // Work in progress
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "perf",
+        "test",
+        "build",
+        "ci",
+        "chore",
+        "revert",
       ],
     ],
-    "type-case": [2, "always", "lower-case"],
-    "subject-case": [
-      2,
-      "never",
-      ["sentence-case", "start-case", "pascal-case", "upper-case"],
-    ],
+    "scope-empty": [2, "never"],
     "subject-empty": [2, "never"],
     "subject-full-stop": [2, "never", "."],
-    "subject-min-length": [2, "always", 3],
-    "subject-max-length": [2, "always", 100],
-    "body-max-line-length": [2, "always", 100],
-    "footer-max-line-length": [2, "always", 100],
+    "header-max-length": [2, "always", 100],
+    "body-leading-blank": [1, "always"],
+    "footer-leading-blank": [1, "always"],
+  },
+  parserPreset: {
+    parserOpts: {
+      issuePrefixes: ["PROJ-"],
+    },
   },
 };
